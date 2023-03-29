c549795316f458a02d990e52e5558bea8169363d
paullabonne <labonnepaul@gmail.com>
2023-03-29 19:01:09 +0200

    updated the data in the package

--- R/data.R
+++ R/data.R
@@ -8,16 +8,6 @@
 #' \insertRef{schaap_genome-wide_2013}{BayesMultiMode}
 "d4z4"
 
-#' DNA Macrosatellite repeat msr5p (4q).
-#' Locus (hg18): 4q35.2; Unit (kb): 3.3; Encoded product: DUX4.
-#'
-#' @format
-#' A vector of counts with 410 elements.
-#' @importFrom Rdpack reprompt
-#' @references
-#' \insertRef{schaap_genome-wide_2013}{BayesMultiMode}
-"msr5p"
-
 #' DNA Macrosatellite repeat ct47 (4q).
 #' Locus (hg18): 4q35.2; Unit (kb): 3.3; Encoded product: DUX4.
 #'
@@ -28,6 +18,14 @@
 #' \insertRef{schaap_genome-wide_2013}{BayesMultiMode}
 "ct47"
 
+#' Tropical cylcone lifetime maximum intensity
+#' @format
+#' See https://www.ncei.noaa.gov/products/international-best-track-archive
+#' @importFrom Rdpack reprompt
+#' @references
+#' \insertRef{Richardson_Green_1997_RJMCMC}{BayesMultiMode}
+"cyclone"
+
 #' Galaxy series
 #' @format
 #' Velocity at which 82 galaxies in the Corona Borealis region are moving away from our galaxy, scaled by 1000.
@@ -35,19 +33,3 @@
 #' @references
 #' \insertRef{Richardson_Green_1997_RJMCMC}{BayesMultiMode}
 "galaxy"
-
-#' Enzyme series
-#' @format
-#' ...
-#' @importFrom Rdpack reprompt
-#' @references
-#' \insertRef{Richardson_Green_1997_RJMCMC}{BayesMultiMode}
-"enzyme"
-
-#' Acidity series
-#' @format
-#' ...
-#' @importFrom Rdpack reprompt
-#' @references
-#' \insertRef{Richardson_Green_1997_RJMCMC}{BayesMultiMode}
-"acidity"
