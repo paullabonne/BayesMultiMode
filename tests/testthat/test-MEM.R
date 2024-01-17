e5aefdb360f3e53d17874a6a6cbc656fdafbf306
paullabonne <labonnepaul@gmail.com>
2024-01-17 16:17:00 +0100

    removed data argument

--- tests/testthat/test-MEM.R
+++ tests/testthat/test-MEM.R
@@ -8,10 +8,7 @@ test_that("MEM function returns expected results with dist = skew_normal", {
   pars_names = c("eta", "xi", "omega", "alpha")
   dist = "skew_normal"
   
-  data = c(sn::rsn(p[1]*1e3, xi[1], omega[1], alpha[1]),
-           sn::rsn(p[2]*1e3, xi[2], omega[2], alpha[2]))
-  
-  modes = MEM(params, data = data, pars_names = pars_names, dist = dist)
+  modes = MEM(params, pars_names = pars_names, dist = dist)
   expect_equal(abs(sum(modes-xi))<1,  TRUE)
   # the two densities are far appart so the modes should coincide with the location parameters
 })
@@ -32,10 +29,7 @@ test_that("MEM function returns expected results with an arbitrary function", {
     sn::dst(x, pars["mu"], pars["sigma"], pars["xi"], pars["nu"])
   }
   
-  data = c(sn::rst(p[1]*1e3, xi[1], omega[1], alpha = alpha[1], nu = nu[1]),
-           sn::rst(p[2]*1e3, xi[2], omega[2], alpha = alpha[2], nu = nu[2]))
-  
-  modes = MEM(params, pars_names = pars_names, data = data, pdf_func = pdf_func)
+  modes = MEM(params, pars_names = pars_names, pdf_func = pdf_func)
   expect_equal(abs(sum(modes-xi))<1,  TRUE)
   # the two densities are far appart so the modes should coincide with the location parameters
 })
--- tests/testthat/test-fixed_point.R
+++ tests/testthat/test-fixed_point.R
@@ -5,10 +5,9 @@ test_that("fixed_point function returns expected results", {
   sigma = c(1,2)
   p = c(0.2,0.2)
   
-  data = c(rnorm(p[1]*100, mu[1], sigma[1]), rnorm(p[2]*100, mu[2], sigma[2]))
   params = c(eta = p, mu = mu, sigma = sigma)
   pars_names = c("eta", "mu", "sigma")
   
-  modes = fixed_point(params, data, pars_names)
+  modes = fixed_point(params, pars_names)
   expect_equal(round(modes),  mu)
 })
