db38fdf9982096b936a70e9360a39a08af3c8ca3
paullabonne <labonnepaul@gmail.com>
2024-01-22 12:49:52 +0100

    added summary method for modes

--- R/summary_methods.R
+++ R/summary_methods.R
@@ -1,4 +1,4 @@
-#' Summary method for for \code{BayesMode} objects
+#' Summary method for \code{BayesMode} objects
 #' 
 #' @param object An object of class \code{BayesMode}.
 #' @param ... Not used.
@@ -18,4 +18,31 @@ summary.BayesMode <- function(object, ...) {
 
   cat("\n\n Number of estimated modes and their posterior probabilities:\n")
   tb_nb_modes
+}
+
+
+#' Summary method for \code{Mode} objects
+#' 
+#' @param object An object of class \code{Mode}.
+#' @param ... Not used.
+#' 
+#' @export
+summary.Mode <- function(object, ...) {
+  stopifnot(inherits(object, "Mode"))
+  
+  Nb_m = length(object$mode_estimates)
+  algo = object$algo
+  d = object$dist
+  K = object$K
+  
+  if (is.na(d)) {
+    d = object$dist_type
+  }
+  if (Nb_m == 1) {
+    m = "Mode"
+  } else {
+    m = "Modes"
+  }
+  cat("\n", Nb_m, m, "of a", d,"distribution with", K, "components.")
+  cat("\n Mode estimation using the", algo,"algorithm.")
 }
